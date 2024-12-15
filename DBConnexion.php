d24dbd6faadb95acacc1634bda45a364e5aa0a97
Mohammed reida <reidamohammed3@gmail.com>
2024-12-15 17:34:33 +0100

    create the DBConnexion fille and open the connexion between the dataBase and the php fille

---
+++ DBConnexion.php
@@ -0,0 +1,11 @@
+<?php 
+
+$server_name = "localhost";
+$user_name = "root";
+$password = "";
+$dataBase_name="Ultimate_Team";
+
+    $conn = mysqli_connect($server_name, $user_name, $password, $dataBase_name);
+
+    
+?>
