e97be30dd113fe7004b2bdb21fc3f359bb6ccfb7
Mohammed reida <reidamohammed3@gmail.com>
2024-12-19 16:00:07 +0100

    make some changes in the player table

--- script.sql
+++ script.sql
@@ -43,7 +43,7 @@ CREATE Table players(
     id_img INT,
     nationality_id INT,
     club_id INT,
-    posiition enum('RW','ST','CM','CB','LW','GK','CDM','LB','RB'),
+    posiition enum('RW','ST','CM1','CM2','CM3','CB1','CB2','LW','GK','CDM','LB','RB'),
     rating INT,
     pace INT,
     shooting INT,
@@ -56,33 +56,41 @@ CREATE Table players(
     FOREIGN KEY (id_img) REFERENCES images (id_img)
 );
 
-INSERT INTO nationalitys (nationality, flaag) VALUES
-('Argentina', 'https://cdn.sofifa.net/flags/ar.png'),
-('Portugal', 'https://cdn.sofifa.net/flags/pt.png'),
-('Belgium', 'https://cdn.sofifa.net/flags/be.png'),
-('France', 'https://cdn.sofifa.net/flags/fr.png'),
-('Netherlands', 'https://cdn.sofifa.net/flags/nl.png'),
-('Germany', 'https://cdn.sofifa.net/flags/de.png'),
-('Brazil', 'https://cdn.sofifa.net/flags/br.png'),
-('Egypt', 'https://cdn.sofifa.net/flags/eg.png'),
-('Norway', 'https://cdn.sofifa.net/flags/no.png'),
-('Croatia', 'https://cdn.sofifa.net/flags/hr.png'),
-('Canada', 'https://cdn.sofifa.net/flags/ca.png'),
-('Slovenia', 'https://cdn.sofifa.net/flags/si.png'),
-('England', 'https://cdn.sofifa.net/flags/gb-eng.png'),
-('Morocco', 'https://cdn.sofifa.net/flags/ma.png'),
-('Italy', 'https://cdn.sofifa.net/flags/it.png');
+INSERT INTO clubs (club, id_img) VALUES
+('Paris Saint-Germain', 63),
+('Real Madrid', 62),
+('FC Barcelona', 65),
+('Chelsea', 62),
+('Bayern Munich', 64),
+('Manchester City', 65),
+('Liverpool', 66),
+('Juventus', 63),
+('Inter Milan', 62),
+('Ajax', 64);
 
-INSERT INTO players (
-    nom, id_img, position, id_img, flag, club, logo, rating, pace, shooting, passing, dribbling, defending, physical
-) VALUES
-    ('Lionel Messi', 'https://cdn.sofifa.net/players/158/023/25_120.png', 'RW', 'Argentina', 'https://cdn.sofifa.net/flags/ar.png', 'Inter Miami', 'https://cdn.sofifa.net/meta/team/239235/120.png', 93, 85, 92, 91, 95, 35, 65),
-    ('Cristiano Ronaldo', 'https://cdn.sofifa.net/players/020/801/25_120.png', 'ST', 'Portugal', 'https://cdn.sofifa.net/flags/pt.png', 'Al Nassr', 'https://cdn.sofifa.net/meta/team/2506/120.png', 91, 84, 94, 82, 87, 34, 77),
-    ('Kevin De Bruyne', 'https://cdn.sofifa.net/players/192/985/25_120.png', 'CM', 'Belgium', 'https://cdn.sofifa.net/flags/be.png', 'Manchester City', 'https://cdn.sofifa.net/players/239/085/25_120.png', 91, 74, 86, 93, 88, 64, 78),
-    ('Kylian Mbappé', 'https://cdn.sofifa.net/players/231/747/25_120.png', 'ST', 'France', 'https://cdn.sofifa.net/flags/fr.png', 'Real Madrid', 'https://cdn.sofifa.net/meta/team/3468/120.png', 92, 97, 89, 80, 92, 39, 77),
-    ('Virgil van Dijk', 'https://cdn.sofifa.net/players/203/376/25_120.png', 'CB', 'Netherlands', 'https://cdn.sofifa.net/flags/nl.png', 'Liverpool', 'https://cdn.sofifa.net/meta/team/8/120.png', 90, 75, 60, 70, 72, 92, 86),
-    ('Antonio Rudiger', 'https://cdn.sofifa.net/players/205/452/25_120.png', 'CB', 'Germany', 'https://cdn.sofifa.net/flags/de.png', 'Real Madrid', 'https://cdn.sofifa.net/meta/team/3468/120.png', 88, 82, 55, 73, 70, 86, 86),
-    ('Neymar Jr', 'https://cdn.sofifa.net/players/190/871/25_120.png', 'LW', 'Brazil', 'https://cdn.sofifa.net/flags/br.png', 'Al-Hilal', 'https://cdn.sofifa.net/meta/team/7011/120.png', 90, 91, 83, 86, 94, 37, 61),
-    ('Mohamed Salah', 'https://cdn.sofifa.net/players/192/985/25_120.png', 'RW', 'Egypt', 'https://cdn.sofifa.net/flags/eg.png', 'Liverpool', 'https://cdn.sofifa.net/meta/team/8/120.png', 89, 93, 87, 81, 90, 45, 75),
-    ('Joshua Kimmich', 'https://cdn.sofifa.net/players/212/622/25_120.png', 'CM', 'Germany', 'https://cdn.sofifa.net/flags/de.png', 'Bayern Munich', 'https://cdn.sofifa.net/meta/team/503/120.png', 89, 70, 75, 88, 84, 84, 81),
-    ('Jan Oblak', 'https://cdn.sofifa.net/players/200/389/25_120.png', 'GK', 'Slovenia', 'https://cdn.sofifa.net/flags/si.png', 'Atletico Madrid', 'https://cdn.sofifa.net/meta/team/7980/120.png', 91, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO goalkeepers (nom, id_img, nationality_id, club_id, posiition, rating, diving, handling, kicking, reflexes, speed, positioning) VALUES
+('Yassine Bounou', 62, 1, 22, 'GK', 88, 90, 85, 80, 89, 65, 88),
+('Thibaut Courtois', 63, 2, 24, 'GK', 90, 92, 89, 85, 91, 60, 88),
+('Ederson Moraes', 64, 3, 21, 'GK', 89, 88, 87, 91, 89, 63, 88),
+('Keylor Navas', 65, 4, 25, 'GK', 88, 87, 86, 84, 88, 59, 86),
+('Manuel Neuer', 66, 5, 23, 'GK', 91, 91, 89, 88, 90, 56, 90),
+('Alisson Becker', 62, 6, 26, 'GK', 90, 89, 90, 88, 90, 57, 88),
+('Marc-André ter Stegen', 63, 7, 29, 'GK', 90, 91, 87, 85, 89, 62, 89),
+('Jan Oblak', 64, 8, 30, 'GK', 92, 93, 91, 83, 90, 59, 91),
+('Hugo Lloris', 65, 9, 27, 'GK', 88, 85, 86, 82, 88, 60, 85),
+('Gianluigi Donnarumma', 66, 10, 28, 'GK', 89, 87, 85, 86, 88, 58, 87);
+
+
+INSERT INTO players (nom, id_img, nationality_id, club_id, posiition, rating, pace, shooting, pasing, dribbling, defending, physical) VALUES
+('Lionel Messi', 62, 11, 21, 'RW', 93, 85, 92, 91, 95, 40, 70),
+('Cristiano Ronaldo', 63, 7, 23, 'ST', 91, 87, 93, 82, 89, 40, 78),
+('Neymar Jr', 64, 3, 24, 'LW', 91, 91, 84, 86, 95, 32, 61),
+('Kevin De Bruyne', 65, 14, 26, 'CM3', 91, 76, 86, 93, 88, 64, 78),
+('Virgil van Dijk', 66, 9, 27, 'CB1', 90, 71, 60, 71, 72, 91, 86),
+('Kylian Mbappé', 62, 12, 22, 'ST', 92, 97, 88, 78, 92, 42, 76),
+('Mohamed Salah', 63, 8, 29, 'RW', 91, 93, 88, 81, 90, 45, 70),
+('Robert Lewandowski', 64, 10, 28, 'ST', 92, 78, 93, 82, 86, 40, 82),
+('Sergio Ramos', 65, 4, 30, 'CB2', 89, 70, 60, 75, 75, 88, 85),
+('Luka Modrić', 66, 13, 25, 'CM2', 89, 74, 78, 91, 90, 71, 64);
+
+
